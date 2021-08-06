739d2b29242c30c958a05302855c62e55f467972
haowei <haowei_web@163.com>
2021-08-06 21:01:19 +0800

    项目开发完毕

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // 导入koa2
-let koa = require('koa')
+let koa = require("koa")
 // 导入中间件
-let koa_response_duration = require('./middleware/koa_response_duration')
-let koa_response_header = require('./middleware/koa_response_header')
-let koa_response_data = require('./middleware/koa_response_data')
+let koa_response_duration = require("./middleware/koa_response_duration")
+let koa_response_header = require("./middleware/koa_response_header")
+let koa_response_data = require("./middleware/koa_response_data")
 // 注册koa2实例
 let app = new koa()
 // 挂载对应的中间件
@@ -14,4 +14,11 @@ app.use(koa_response_header)
 // 处理业务逻辑中间件，读取某个json文件的数据
 app.use(koa_response_data)
 // 监听端口
-app.listen(8888)
+app.listen(8888, () => {
+    console.log("服务器启动成功，请访问localhost:8888")
+})
+
+const WebSocketService = require("./service/web_socket_service")
+// 开启服务端的监听，监听客户端的连接
+// 当某一个客户端连接成功之后，就会对这个客户端进行 message 事件的监听
+WebSocketService.listen()
